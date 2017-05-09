35498d7a0f234d12796beefd21f368700ed0919b
Zinzidane <ya-gans@mail.ru>
2017-05-09 16:51:58 +0200

    Прогрессивное улучшение (форма поиска)
    Форма поиска открыта по умолчанию при выключенном js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 var link = document.querySelector(".search-btn");
 var popup = document.querySelector(".search-form");
 var dayIn = popup.querySelector("[name=day-in]");
-
+	
+popup.classList.add("search-form-hide");
 link.addEventListener("click", function(event) {
     event.preventDefault();
     popup.classList.toggle("search-form-show");
